f737169c02a3cfc99d42aefd389a2e2b64185ee7
soyyoonn <warminhap@gmail.com>
2023-01-31 21:16:14 +0900

    230131 21:15

--- sy_chat.py
+++ sy_chat.py
@@ -2,6 +2,7 @@
 
 import sys
 import pymysql
+import json
 from socket import *
 from threading import *
 from PyQt5.QtWidgets import *
@@ -13,14 +14,41 @@ form_class = uic.loadUiType('./chatclient.ui')[0]
 
 class Main(QMainWindow, form_class):
     client_socket = None
-
     def __init__(self, ip, port):
         super().__init__()
         self.setupUi(self)
+        self.stackedWidget.setCurrentIndex(0)
         self.initialize_socket(ip, port)
         self.listen_thread()
         self.btn_send.clicked.connect(self.send_chat)
+        self.btn_enter.clicked.connect(self.chat_in)
+        self.btn_exit.clicked.connect(self.chat_out)
+        self.btn_back.clicked.connect(self.move_chatlist)
+        self.lineEdit.returnPressed.connect(self.chat_in)
         self.sendmessage.returnPressed.connect(self.send_chat)
+        self.chatroom.itemDoubleClicked.connect(self.move_chat)
+
+
+    def chat_in(self):
+        self.senders_name = self.lineEdit.text()
+        if self.senders_name == '':
+            QMessageBox.information(self, '알림', '사용자 이름을 입력해주세요')
+            return
+        self.stackedWidget.setCurrentIndex(1)
+        self.client_socket.send('002'.encode('utf-8'))
+
+
+    def chat_out(self):
+        self.stackedWidget.setCurrentIndex(0)
+        self.lineEdit.clear()
+        self.receivemessage.clear()
+
+    def move_chatlist(self):
+        self.stackedWidget.setCurrentIndex(1)
+
+    def move_chat(self):
+        self.stackedWidget.setCurrentIndex(2)
+
 
     def initialize_socket(self, ip, port):
         ''' tcp socket을 생성하고 server와 연결 '''
@@ -29,34 +57,61 @@ class Main(QMainWindow, form_class):
         remote_port = port
         self.client_socket.connect((remote_ip, remote_port))
 
+
+    # def chat_data(self):
+    #     conn = pymysql.connect(host='10.10.21.101', port=3306, user='chat', password='0000', db='network')
+    #     cursor = conn.cursor()
+    #     cursor.execute(
+    #         f"SELECT * FROM chat WHERE send='{self.senders_name}' and IP='{ip}'")
+    #     self.chattext = cursor.fetchall()
+    #     print(self.chattext)
+    #     conn.commit()
+    #     conn.close()
+    #     for i in range(len(self.chattext)):
+    #         self.receivemessage.addItem(self.chattext[i][0] + ':' + self.chattext[i][1])
+
+
+
     def send_chat(self):
         ''' message를 전송하는 버튼 콜백 함수 '''
-        senders_name = self.lineEdit.text()
-        # if senders_name == '':
-        #     QMessageBox.information(self, '알림', '사용자 이름을 입력해주세요')
-        #     return
         data = self.sendmessage.text()
         # if data == '':
         #     QMessageBox.information(self, '알림', '메시지를 입력해주세요')
         #     return
-        message = (senders_name + ' : ' + data).encode('utf-8')
-        self.receivemessage.addItem(message.decode('utf-8'))
+        message = (self.senders_name + ':' + data + ':' + '001').encode('utf-8')
+        self.receivemessage.addItem(self.senders_name + ':' + data)
         self.client_socket.send(message)
         self.sendmessage.clear()
+        # self.receivemessage.scrollToBottom()
         return 'break'
+
 
     def listen_thread(self):
         ''' 데이터 수신 Thread를 생성하고 시작한다 '''
-        t = Thread(target=self.receive_message, args=(self.client_socket,))
+        t = Thread(target=self.receive_message, args=(self.client_socket,), daemon=True)
         t.start()
+
 
     def receive_message(self, so):
         while True:
-            buf = so.recv(256)
-            if not buf: # 연결이 종료됨
-                break
-            self.receivemessage.addItem(buf.decode('utf-8'))
+            try:
+                buf = so.recv(1024)  # 서버로부터 문자열 수신
+                chat_msg = buf.decode('utf-8')
+                print(chat_msg,"jhjjhhj")
+                if not buf: # 문자열 없으면 연결이 종료됨
+                    break
+                if chat_msg[-3:] == '001':
+                    self.receivemessage.addItem(chat_msg[:-4])
+                elif chat_msg[-3:] == '002':
+                    chatlog = json.loads(chat_msg[:-3])
+                    for i in chatlog:
+                        self.receivemessage.addItem(i[0] + ':' + i[1])
+                        print(i,"hgtfr")
+                    self.receivemessage.scrollToBottom()
+            except:
+                pass
         so.close()
+
 
 if __name__ == "__main__":
     ip = input("server IP addr: ")
--- sy_server.py
+++ sy_server.py
@@ -1,5 +1,6 @@
 # threading 모듈을 이용한 TCP 멀티 채팅 서버 프로그램
 
+import json
 import pymysql
 from socket import *
 from threading import *
@@ -13,65 +14,82 @@ class MultiChatServer:
         self.s_sock = socket(AF_INET, SOCK_STREAM)
         self.ip ='10.10.21.101'
         self.port = 9180
-        self.s_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.s_sock.bind((self.ip, self.port))
+        self.s_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  # 주소 재사용
+        self.s_sock.bind((self.ip, self.port))    # 연결 대기
         print("클라이언트 대기 중...")
-        self.s_sock.listen(100)
-        self.accept_client()
+        self.s_sock.listen(100)   # 최대 접속인원 100명
+        self.accept_client()    # 클라이언트와 연결
 
     # 연결 클라이언트 소켓을 목록에 추가하고 스레드를 생성하여 데이터를 수신한다
     def accept_client(self):
+        self.add = []
         while True:
             client = c_socket, (ip, port) = self.s_sock.accept()
             if client not in self.clients:
                 self.clients.append(client)   # 접속된 소켓을 목록에 추가
             print(ip, ':', str(port), ' 가 연결되었습니다')
             print(self.clients)
-            cth = Thread(target=self.receive_messages, args=(c_socket,))  # 수신 스레드
+            self.add.insert(0, ip)
+            self.add.insert(1, port)
+            cth = Thread(target=self.receive_messages, args=(c_socket,), daemon=True)  # 수신 스레드
             cth.start()   # 스레드 시작
-            # list = []
-            # list.append(0, ip)
-            # list.append(1, str(port))
 
     # 데이터를 수신하여 모든 클라이언트에게 전송한다
     def receive_messages(self, c_socket):
-        now = datetime.now()
-        now_time = now.strftime('%Y-%m-%d %H:%M:%S')
-        print(now_time)
+        # now = datetime.now()
+        # now_time = now.strftime('%Y-%m-%d %H:%M:%S')
+        # print(now_time)
         while True:
             try:
-                incoming_message = c_socket.recv(256)
+                incoming_message = c_socket.recv(1024)
                 if not incoming_message: # 연결이 종료됨
                     break
             except:
                 continue
-            else:
+            else:  # 예외가 발생하지 않아 except 실행되지 않을시 실행
                 self.final_received_message = incoming_message.decode('utf-8')
                 print(self.final_received_message)
+                # self.final_received_message = json.loads(incoming_message.decode('utf-8'))
+                # json.dumps(self.final_received_message)
+                print(self.final_received_message)
+                # ---------------------------------------------------------------------------
+                if self.final_received_message[-3:] == '001':
+                    self.m = self.final_received_message.split(':')
+                    print(type(self.m[0]), self.m[0])
+                    print(type(self.m[1]), self.m[1])
+                    print(self.add[0])
+                    print(self.add[1])
+                    conn = pymysql.connect(host='10.10.21.101', port=3306, user='chat', password='0000', db='network')
+                    cursor = conn.cursor()
+                    cursor.execute(
+                        f"INSERT INTO chat (send, message, time, IP, PORT) VALUES('{self.m[0]}','{self.m[1]}',{'now()'},'{self.add[0]}','{self.add[1]}')")
+                    conn.commit()
                 self.send_all_clients(c_socket)
-            # a = self.final_received_message.split(':')
-            # print(a)
-            # print(type(a[0]), a[0])
-            # print(type(a[1]), a[1])
-            # print(self.list[0])
-            # print(self.list[1])
-            # conn = pymysql.connect(host='10.10.21.101', port=3306, user='chat', password='0000', db='network')
-            # c = conn.cursor()
-            # c.execute(
-            #     f"INSERT INTO chat (send, message, time, IP, PORT) VALUES('{a[0]}','{a[1]}',{'now()'},'{self.list[0]}','{self.list[1]}')")
-            # conn.commit()
-            # conn.close()
+        c_socket.close()
 
     # 송신 클라이언트를 제외한 모든 클라이언트에게 메시지 전송
     def send_all_clients(self, senders_socket):
-        for client in self.clients: # 목록에 있는 모든 소켓에 대해
+        for client in self.clients:  # 목록에 있는 모든 소켓에 대해
             socket, (ip, port) = client
             if socket is not senders_socket: # 송신 클라이언트는 제외
                 try:
-                    socket.sendall(self.final_received_message.encode('utf-8'))
+                    if self.final_received_message[-3:] == '001':
+                        socket.sendall((self.final_received_message).encode('utf-8'))
                 except:  # 연결 종료
                     self.clients.remove(client)   # 소켓 제거
                     print("{}, {} 연결이 종료되었습니다".format(ip, port))
+            else:
+                if self.final_received_message[-3:] == '002':
+                    conn = pymysql.connect(host='10.10.21.101', port=3306, user='chat', password='0000', db='network')
+                    cursor = conn.cursor()
+                    cursor.execute(
+                        f"SELECT send, message FROM chat")
+                    self.chattext = cursor.fetchall()
+                    print(self.chattext)
+                    conn.close()
+                    chattext = json.dumps(self.chattext)
+                    print(chattext)
+                    senders_socket.sendall((chattext + '002').encode())
 
 
 if __name__ == "__main__":
