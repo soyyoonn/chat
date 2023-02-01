806485c3324c24e2be10e181cf1049493ed64e5c
lsgyeong <tjd12rud12@naver.com>
2023-02-01 20:56:49 +0900

    연습용

--- GUI_ChatServer_Multi.py
+++ GUI_ChatServer_Multi.py
@@ -4,74 +4,116 @@
 from socket import *
 from threading import *
 import pymysql
+import json
+
 
 class ChatserverMulti:
 
     # 소켓 생성, 연결되면 accept_client() 호출
     def __init__(self):
-        self.clients=[]               # 접속된 클라이언트 소켓 목록
-        self.final_received_message='' # 최종 수신 메시지
-        self.s_sock=socket(AF_INET,SOCK_STREAM)           # 소켓생성
-        self.ip=''
-        self.port=9523
-        self.s_sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1) # 주소 재사용
-        self.s_sock.bind((self.ip,self.port))             # 연결대기
+        self.clients = []  # 접속된 클라이언트 소켓 목록
+        self.nickname=[]   # 닉네임 넣기
+        self.final_received_message = ''  # 최종 수신 메시지
+        self.s_sock = socket(AF_INET, SOCK_STREAM)  # 소켓생성
+        self.ip = ''
+        self.port = 9028
+        self.s_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  # 주소 재사용
+        self.s_sock.bind((self.ip, self.port))  # 연결대기
         print('클라이언트 대기중...')
-        self.s_sock.listen(3)                             # 최대 접속인원 3명
+        self.s_sock.listen(100)  # 최대 접속인원 3명
         # accept_clinet()를 호출하여 클라이언트와 연결
         self.accept_client()
 
     # 연결 클라이언트 소켓을 목록에 추가하고 스레드를 생성하여 데이터 수신
     def accept_client(self):
-        self.list=[]
+        self.list = []
         while True:
-            client=c_socket,(ip,port)=self.s_sock.accept()
+            client = c_socket, (ip, port) = self.s_sock.accept()
             print(client)
-            if client not in self.clients:      # 만약에 연결클라이언트가 접속된 클라이어트 소켓목록에 없으면
-                self.clients.append(client)     # 접속된 소켓을 목록에 추가
+            if client not in self.clients:  # 만약에 연결클라이언트가 접속된 클라이어트 소켓목록에 없으면
+                self.clients.append(client)  # 접속된 소켓을 목록에 추가
             print(f'{ip}:{str(port)}가 연결되었습니다')
-            self.list.insert(0,ip)
-            self.list.insert(1,port)
-            cth=Thread(target=self.receive_message, args=(c_socket,), daemon=True)    # 수신스레드
-            cth.start()    # 스레드 시작
+            self.list.insert(0, ip)
+            self.list.insert(1, port)
+            cth = Thread(target=self.receive_message, args=(c_socket,), daemon=True)  # 수신스레드
+            cth.start()  # 스레드 시작
 
     # 데이터를 수신하여 모든 클라이언트에게 전송
-    def receive_message(self,c_socket):
+    def receive_message(self, c_socket):
         while True:
             try:
-                incoming_message=c_socket.recv(256)
-                if not incoming_message: # 연결이 종료됨
+                incoming_message = c_socket.recv(2048)
+                if not incoming_message:
+                    c_socket.close()
                     break
-            except:
-                continue
-            else: # 예외가 발생하지 않아 except절을 실행하지 않았을 경우 실행됨.
-                self.final_received_message=incoming_message.decode('utf-8')
+            except Exception as e:
+                print(e)
+                break
+            else:  # 예외가 발생하지 않아 except절을 실행하지 않았을 경우 실행됨.
+                self.final_received_message = incoming_message.decode()
                 print(self.final_received_message)
-                self.send_all_client(c_socket)
-                self.db_save()
-        # c_socket.close()
+                print(bool(self.final_received_message[-3:] == '006'))
+                if self.final_received_message == '001':
+                    conn = pymysql.connect(host='10.10.21.101', port=3306, user='chatt', password='0000', db='network')
+                    c = conn.cursor()
+                    c.execute(f'SELECT send,message FROM network.chat where send not in ("알림")')
+                    self.chat_db = c.fetchall()
+                    conn.close()
+                    aa = json.dumps(self.chat_db)
+                    c_socket.send((aa + '001').encode())
+
+                elif self.final_received_message[-3:] == '003':
+                    self.send_all_client(c_socket,2)
+                    # 접속명단 확인
+                    if self.final_received_message[:-3] not in self.nickname:
+                        self.nickname.append(self.final_received_message[:-3])
+                        print(self.nickname)
+
+                elif self.final_received_message[-3:] == '002':
+                    self.send_all_client(c_socket,1)
+                    self.db_save()
+
+                elif self.final_received_message[-3:] == '004':
+                    self.roomname=self.final_received_message[:-3]
+
+                elif self.final_received_message == '005':
+                    self.send_all_client_chat_in(c_socket)
+
+                elif self.final_received_message[-3:] == '006':
+                    self.send_all_client(c_socket,5)
+
+
     def db_save(self):
-        a = self.final_received_message.split(':')
-        print(type(a[0]), a[0])
-        print(type(a[1]), a[1])
-        print(self.list[0])
-        print(self.list[1])
+        a = self.final_received_message[:-3].split(':')
         conn = pymysql.connect(host='10.10.21.101', port=3306, user='chatt', password='0000', db='network')
         c = conn.cursor()
-        c.execute(f"insert into `network`.`chat` (send, message, `time`, IP, PORT) values ('{a[0]}','{a[1]}',{'now()'},'{self.list[0]}','{self.list[1]}')")
+        c.execute(
+            f"insert into `network`.`chat` (send, message, `time`, IP, PORT, roomname) \
+            values ('{a[0]}','{a[1]}',{'now()'},'{self.list[0]}','{self.list[1]}','{self.roomname}')")
         conn.commit()
         conn.close()
 
     # 모든 클라이언트에게 메시지 전송
-    def send_all_client(self,senders_socket):
-        for client in self.clients:             # 목록에 있는 모든 소켓에 대해
-            socket, (ip,port) = client
-            # if socket is not senders_socket:    # 송신 클라이언트 제외
+    def send_all_client(self, sender_socket, num):
+        check=['001','002','003','004','005','006']
+        for client in self.clients:  # 목록에 있는 모든 소켓에 대해
+            socket, (ip, port) = client
             try:
-                socket.sendall(self.final_received_message.encode())
-            except: # 연결종료
-                self.clients.remove(client) # 소켓 제거
-                print(f'{ip}:{port} 연결이 종료되었습니다')
+                message=json.dumps(self.final_received_message[:-3])
+                self.message_end=message+check[num]
+                socket.sendall(self.message_end.encode())
+            except:
+                self.clients.remove(client)
 
-if __name__=='__main__':
+   # 현재 접속 인원 전체 클라이언트에게 보내기
+    def send_all_client_chat_in(self, sender_socket):
+        for client in self.clients:  # 목록에 있는 모든 소켓에 대해
+            socket, (ip, port) = client
+            try:
+                message = json.dumps(self.nickname)
+                socket.sendall((message +'005').encode())
+            except:
+                self.clients.remove(client)
+
+if __name__ == '__main__':
     ChatserverMulti()
--- GUI_chatclient2.py
+++ GUI_chatclient2.py
@@ -1,9 +1,11 @@
 # GUI 채팅 클라이언트
+import json
 from socket import *
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import threading
+import time
 
 form_class = uic.loadUiType("chat.ui")[0]
 
@@ -17,58 +19,89 @@ class WindowClass(QMainWindow, form_class) :
         self.send_btn.clicked.connect(self.send_chat)    # 전송버튼 누를시 서버에 메시지 전송
         self.go_btn.clicked.connect(self.chat_page)      # 채팅방 입장 눌렀을 시 채팅으로 이동
         self.exit_btn.clicked.connect(self.main_page)    # 채팅방 나가기 눌렀을시 닉네임입력칸으로 이동
-
-        cThread = threading.Thread(target=self.receive_message, args=(self.client_socket,))
-        cThread.daemon = True  # 메인스레드가 끝날때 같이 종료됨, 아니면 계속 스레드 돌아감
-        cThread.start()
+        self.send_line.returnPressed.connect(self.send_chat)
+        self.chat_list.itemClicked.connect(self.itme_widget)
 
         self.stackedWidget.setCurrentIndex(0)
+    def itme_widget(self):
+        self.row = self.chat_list.currentRow()
+        self.item = self.chat_list.currentItem()
 
     def chat_page(self):
+        print(self.row)
         self.stackedWidget.setCurrentIndex(1)
-        alarm = (f'알림:"{self.name_line.text()}" 님이 채팅방에 입장하셨습니다').encode('utf-8')
-        self.client_socket.send(alarm)                      # 서버로 전송
+        if self.row == 0:
+            self.chat_title.setText('채팅방1')
+            self.label_5.setText(f'{self.name_line.text()}님 환영합니다')
+            self.client_socket.send('001'.encode())                             # 001 : 이전내역
+            self.client_socket.send((self.name_line.text() + '003').encode())   # 003 : 접속 닉네임
+            time.sleep(0.5)
+            self.client_socket.send((self.item.text() + '004').encode())        # 004 : 채팅방이름(DB저장)
+            time.sleep(0.5)
+            self.client_socket.send(('005').encode())                           # 005 : 현재접속자 명단
+            self.susin_list.scrollToBottom()
 
-    def main_page(self,sock):
+        else:
+            pass
+    def main_page(self):
         self.name_line.clear()                              # 닉네임 삭제
-        self.recv_bra.clear()                               # 텍스트 브라우저 내용 삭제
+        self.susin_list.clear()
+        self.listWidget_2.clear()
         self.stackedWidget.setCurrentIndex(0)
 
     # 소켓생성 및 서버와 연결
     def initialize_socket(self):
         ip = '10.10.21.124'
-        port = 9523
+        port = 9014
         self.client_socket=socket(AF_INET,SOCK_STREAM)
         self.client_socket.connect((ip,port))
-        print(self.client_socket)
 
     # message를 전송하는 콜백함수
     # 송신메시지 창에서 메시지를 읽어 전송
     def send_chat(self):
         senders_name=self.name_line.text()                   # 사용자 이름 받아오기
-        data=self.send_line.toPlainText()                    # 송신메시지 받아오기
-        # a=self.recv_bra.append(f'{senders_name} : {data}')   # 수신메시지 텍스트 브라우저에 띄우기
-        message=(f'{senders_name}:{data}').encode('utf-8') # 서버로 사용자이름, 송신메시지 보낼 준비
-        self.client_socket.send(message)                     # 서버로 전송
+        data=self.send_line.text()                           # 송신메시지 받아오기
+        message=(f'{senders_name}:{data}')                   # 서버로 사용자이름, 송신메시지 보낼 준비
+        self.client_socket.send((message+'002').encode())    # 서버전송 : 메시지
         self.send_line.clear()                               # 작성한 송신메시지 삭제
 
     # 소켓에서 메시지를 읽어서 수신메시지 창에 표시
     def receive_message(self,so):
-        print(so)
         while True:
-            buf=so.recv(256)                                # 서버로부터 받은 메시지가 있으면
-            print(buf)
-            recv_data=buf.decode()                          # 메시지 문자열로 변환
-            print(recv_data)
-            a=self.recv_bra.append(f'{recv_data}')            # 수신메시지 텍스트 브라우저에 띄우기
-            self.recv_bra.scrollToAnchor(a)                   # 앵커 위치로 스크롤 함.
-            if not buf: # 연결 종료
-                print('연결이 종료되었습니다')
-                pass
+            try:
+                buf = so.recv(2048)
+                if not buf:
+                    # so.close()
+                    break
+            except Exception as e:
+                print(e)
+                # break
+            else:  #
+                recv_data = buf.decode()  # 메시지 문자열로 변환
+                if recv_data[-3:] == '001':  # 지난 채팅내역 띄우기
+                    j_data = json.loads(recv_data[:-3])
+                    for i in j_data:
+                        self.susin_list.addItem(f'{i[0]}:{i[1]}')
+                    self.susin_list.addItem(f'------------- 이전채팅내역 -------------')
+                elif recv_data[-3:] == '003':  # 입장알리기
+                    j_data1 = json.loads(recv_data[:-3])
+                    self.susin_list.addItem(f'---------- {j_data1}님 채팅방 입장 ----------')
+                elif recv_data[-3:] == '002':  # 실시간 채팅 내역 띄우기
+                    j_data2 = json.loads(recv_data[:-3])
+                    self.susin_list.addItem(j_data2)
+                elif recv_data[-3:] == '005':  # 현재 접속자 명단
+                    self.listWidget_2.clear()
+                    j_data3 = json.loads(recv_data[:-3])
+                    print(j_data3)
+                    for i in range(len(j_data3)):
+                        self.listWidget_2.addItem(j_data3[i])
+
 
 if __name__=='__main__':
     app = QApplication(sys.argv)     #QApplication : 프로그램을 실행시켜주는 클래스
     myWindow = WindowClass()         #WindowClass의 인스턴스 생성
     myWindow.show()                  #프로그램 화면을 보여주는 코드
+    cThread = threading.Thread(target=myWindow.receive_message, args=(myWindow.client_socket,))
+    cThread.daemon = True  # 메인스레드가 끝날때 같이 종료됨, 아니면 계속 스레드 돌아감
+    cThread.start()
     app.exec_()                      #프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
-
